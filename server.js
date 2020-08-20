8fa660ae0463b06877f4c4a5cbcc88184ae161e9
Nicholas Roberto <njr444@gmail.com>
2020-08-20 13:44:39 -0700

    updated signin route to connect to database.

--- server.js
+++ server.js
@@ -23,15 +23,27 @@ app.get('/', (req, res) => {
   res.send(database.users)
 })
 app.post('/signin', (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0])
-  } else {
-    res.status(400).json('Error logging in')
-  }
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('Unable to get user'))
+      } else {
+        res.status(400).json('Wrong Credentials')
+      }
+    })
+    .catch(err => res.status(400).json('Wrong Credentials'))
 })
+
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body
   const hash = bcrypt.hashSync(password)
