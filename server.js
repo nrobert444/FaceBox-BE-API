dbed83b5d30d08adfc68806806902da4e13f7ac6
Nicholas Roberto <njr444@gmail.com>
2020-08-20 12:27:18 -0700

    updated register, profile/:id, and image routes with new sql database.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs')
 const cors = require('cors')
 const knex = require('knex')
 
-const postgres = knex({
+const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
@@ -13,7 +13,11 @@ const postgres = knex({
   }
 })
 
-postgres.select('*').from('users')
+db.select('*')
+  .from('users')
+  .then(data => {
+    console.log(data)
+  })
 
 const app = express()
 
@@ -56,44 +60,47 @@ app.post('/signin', (req, res) => {
   }
 })
 app.post('/register', (req, res) => {
-  const { email, name } = req.body
-
-  database.users.push({
-    id: '125',
-    name,
-    email,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length - 1])
+  const { email, name, password } = req.body
+  db('users')
+    .returning('*')
+    .insert({
+      email,
+      name,
+      joined: new Date()
+    })
+    .then(user => {
+      res.json(user[0])
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params
-  let found = false
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true
-      return res.json(user)
-    }
-  })
-  if (!found) {
-    res.status(400).json('User not found')
-  }
+  db.select('*')
+    .from('users')
+    .where({
+      id
+    })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('User not found')
+      }
+    })
+    .catch(err => res.status(400).json('Error getting user'))
 })
+
 app.put('/image', (req, res) => {
   const { id } = req.body
-  let found = false
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true
-      user.entries++
-      return res.json(user.entries)
-    }
-  })
-  if (!found) {
-    res.status(400).json('User not found')
-  }
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('Unable to get count'))
 })
 
 app.listen(3000, () => {
